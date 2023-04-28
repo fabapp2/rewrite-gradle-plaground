e1e218095c0fc0541244d1741491936dfc006ade
Fabian Krüger <fkrueger@vmware.com>
2023-04-28 10:48:29 +0200

    Hacking the Gradle API

--- src/test/java/org/springframework/sbm/build/gradle/ParseGradleFileTest.java
+++ src/test/java/org/springframework/sbm/build/gradle/ParseGradleFileTest.java
@@ -15,59 +15,129 @@
  */
 package org.springframework.sbm.build.gradle;
 
-import org.assertj.core.api.Assertions;
+import com.google.common.jimfs.Configuration;
+import com.google.common.jimfs.Jimfs;
+import org.gradle.tooling.GradleConnector;
+import org.gradle.tooling.ProjectConnection;
+import org.gradle.tooling.internal.consumer.DefaultGradleConnector;
+import org.gradle.tooling.model.GradleProject;
+import org.gradle.tooling.model.build.BuildEnvironment;
 import org.junit.jupiter.api.Test;
 import org.openrewrite.RecipeRun;
 import org.openrewrite.Result;
 import org.openrewrite.SourceFile;
 import org.openrewrite.gradle.GradleParser;
+import org.openrewrite.gradle.toolingapi.OpenRewriteModel;
+import org.openrewrite.gradle.toolingapi.OpenRewriteModelBuilder;
 import org.openrewrite.groovy.tree.G;
 import org.openrewrite.gradle.search.FindDependency;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 /**
  * @author Fabian Krüger
  */
 public class ParseGradleFileTest {
+
+    public static final String GRADLE_KOTLIN_FILE = """
+            plugins {
+            	java
+            	id("org.springframework.boot") version "3.0.6"
+            	id("io.spring.dependency-management") version "1.1.0"
+            }
+                        
+            group = "com.acme"
+            version = "0.0.1-SNAPSHOT"
+            java.sourceCompatibility = JavaVersion.VERSION_17
+                        
+            repositories {
+            	mavenCentral()
+            }
+                        
+            dependencies {
+            	implementation("org.springframework.boot:spring-boot-starter")
+            	implementation("org.springframework.kafka:spring-kafka")
+            	testImplementation("org.springframework.boot:spring-boot-starter-test")
+            	testImplementation("org.springframework.kafka:spring-kafka-test")
+            }
+                        
+            tasks.withType<Test> {
+            	useJUnitPlatform()
+            }
+            """;
+
+    public static final String GRADLE_GROOVY_FILE = """
+            plugins {
+                id 'java'
+                id 'org.springframework.boot' version '2.7.11'
+                id 'io.spring.dependency-management' version '1.0.15.RELEASE'
+            }
+                            
+            group = 'com.example'
+            version = '0.0.1-SNAPSHOT'
+            sourceCompatibility = '17'
+                            
+            repositories {
+                mavenCentral()
+            }
+                            
+            dependencies {
+                implementation 'org.springframework.boot:spring-boot-starter-web'
+                testImplementation 'org.springframework.boot:spring-boot-starter-test'
+            }
+                            
+            tasks.named('test') {
+                useJUnitPlatform()
+            }
+            """;
+
     @Test
     void parseGradleFile() {
-        String gradleFile = """
-                plugins {
-                	id 'java'
-                	id 'org.springframework.boot' version '2.7.11'
-                	id 'io.spring.dependency-management' version '1.0.15.RELEASE'
-                }
-                                
-                group = 'com.example'
-                version = '0.0.1-SNAPSHOT'
-                sourceCompatibility = '17'
-                                
-                repositories {
-                	mavenCentral()
-                }
-                                
-                dependencies {
-                	implementation 'org.springframework.boot:spring-boot-starter-web'
-                	testImplementation 'org.springframework.boot:spring-boot-starter-test'
-                }
-                                
-                tasks.named('test') {
-                	useJUnitPlatform()
-                }
-                """;
 
-        List<G.CompilationUnit> parse = GradleParser.builder().build().parse(gradleFile);
-        RecipeRun run = new FindDependency("org.springframework.boot", "spring-boot-starter-web", null)
-                .run(parse);
-
-        SourceFile after = run.getResults().get(0).getAfter();
-        String result = after.printAll();
-        System.out.println(result);
+        List<G.CompilationUnit> parse = GradleParser.builder().build().parse(GRADLE_GROOVY_FILE);
+        RecipeRun run = new FindDependency("org.springframework.boot", "spring-boot-starter-web", null).run(parse);
         run.getDataTables();
-//
-//        run = new FindDependency("*", "*", null)
-//                .run(parse);
+    }
+
+    @Test
+    void test_renameMe() throws IOException {
+        FileSystem fileSystem = Jimfs.newFileSystem(Configuration.osX());
+        String fileName = "build.gradle.kts";
+        Path pathToStore = fileSystem.getPath("");
+
+
+        Path filePath = pathToStore.resolve(fileName);
+        Files.writeString(filePath, GRADLE_KOTLIN_FILE);
+        assertThat(Files.readString(filePath)).isEqualTo(GRADLE_KOTLIN_FILE);
+        File projectDir = new File(pathToStore.toString());
+//        OpenRewriteModel openRewriteGradleModel = OpenRewriteModelBuilder.forProjectDirectory(projectDir);
+//        assertThat(openRewriteGradleModel.gradleProject().getPlugins()).hasSize(4);
+//        System.out.println(openRewriteGradleModel.gradleProject().getPlugins());
+
+        DefaultGradleConnector connector = (DefaultGradleConnector) GradleConnector.newConnector();
+        GradleConnector gradleConnector = connector.forProjectDirectory(projectDir);
+        gradleConnector.useGradleVersion("7.4.2");
+        ProjectConnection connection = gradleConnector.connect();
+        BuildEnvironment environment = connection.model(BuildEnvironment.class).get();
+        System.out.println("==== Environment");
+        System.out.println(environment.getGradle().getGradleVersion());
+        System.out.println(environment.getGradle().getGradleUserHome());
+        System.out.println(environment.getBuildIdentifier().getRootDir());
+        System.out.println(environment.getJava().getJavaHome());
+
+        System.out.println("==== GradleProject");
+        GradleProject gradleProject = connection.model(GradleProject.class).get();
+        gradleProject.getTasks().getAll().stream()
+                .forEach(t -> System.out.println(t));
+        System.out.println(gradleProject.getBuildScript().getSourceFile());
 
     }
 
